a6d72b9ea6b3f982b1e62c36f120283515b09adf
Ruilu <2983713t@student.gla.ac.uk>
2025-07-26 22:27:04 +0100

    Introduce Gmail API

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import hello_world
 from .views_auth import register, login
+from . import views_auth
 
 urlpatterns = [
     path('hello/', hello_world),
     path('register/', register),
     path('login/', login),
+    path('gmail/auth_url/', views_auth.gmail_auth_url, name='gmail_auth_url'),
+    path('gmail/callback/', views_auth.gmail_callback, name='gmail_callback'),
+    path('gmail/receipts/', views_auth.gmail_receipts, name='gmail_receipts'),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
+
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or config('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or config('GOOGLE_CLIENT_SECRET')
+GOOGLE_REDIRECT_URI = 'http://localhost:8000/api/gmail/callback/'
+GOOGLE_SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'openid', 'email', 'profile']
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,10 +45,34 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',      # allauth need
     'rest_framework',
-    'api',
+    'rest_framework_simplejwt',  # JWT support
     'corsheaders',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'api',                      # my App
 ]
+
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # 默认用户密码
+    'allauth.account.auth_backends.AuthenticationBackend',  # allauth用
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'},
+    }
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +80,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    'allauth.account.middleware.AccountMiddleware',  
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'corsheaders.middleware.CorsMiddleware',
